7d60b8e70998bcbfc74934c76b51270328130c9d
Denis123f <87093333+Denis123f@users.noreply.github.com>
2025-04-10 16:33:49 +0500

    Update mainwindow.h

--- mainwindow.h
+++ mainwindow.h
@@ -1,69 +1,58 @@
-#ifndef MAINWINDOW_H
-#define MAINWINDOW_H
+#ifndef SCANNER_TEXT_H
+#define SCANNER_TEXT_H
 
-#include <QMainWindow>
+
 #include <QApplication>
 #include <QTextEdit>
 #include <QHBoxLayout>
 #include <QPushButton>
-#include <QWidget>
-#include <QScreen>
-#include <QPixmap>
 #include <QPainter>
 #include <QMouseEvent>
-#include <QFont>
-#include <QSize>
-#include <QPainter>
-#include <QPaintEvent>
-#include <QPoint>
-#include <QRect>
-#include <QImage>
 #include <tesseract/baseapi.h>
 #include <leptonica/allheaders.h>
-#include <QPixmap>
 #include <QBuffer>
-#include <QMessageBox>
 #include <QLabel>
 #include <QComboBox>
-// #include <math>
 
 class Painter_for_new_widget : public QWidget{
     Q_OBJECT
 public:
-    QTextEdit* txt_dublikate;
+    Painter_for_new_widget(QWidget *parent = nullptr);
+
+
     void mousePressEvent(QMouseEvent*);
     void mouseMoveEvent(QMouseEvent *);
     void mouseReleaseEvent(QMouseEvent*);
     void closeEvent(QCloseEvent *);
-    Painter_for_new_widget(QWidget *parent = nullptr);
+    void paintEvent(QPaintEvent*);
+
     bool check_paint = false;
-    QString result = "";
     int local_first_click_X, local_first_click_Y, local_out_first_click_X, local_out_first_click_Y;
     int global_first_click_X, global_first_click_Y, global_out_first_click_X, global_out_first_click_Y;
+    QString result = "";
     QSize screen;
-    void paintEvent(QPaintEvent*);
+    QTextEdit* txt_dublikate;
     QString lang;
-
-private slots:
 };
 
 
-class MainWindow : public QWidget
+class Scanner_text : public QWidget
 {
     Q_OBJECT
 
 public:
-    MainWindow(QWidget *parent = nullptr);
-    ~MainWindow();
+    Scanner_text(QWidget *parent = nullptr);
     int x = 20;
     Painter_for_new_widget* window_supple;
-
     QTextEdit* txt;
+
 private:
-    QString lang;
+
     void set_txt_on_window(const QTextEdit&);
     void main_function();
     void closeEvent(QCloseEvent *);
+
+    QString lang;
     QSize screen;
     QStringList st_list;
     QHBoxLayout* box_h;
@@ -73,14 +62,15 @@ private:
     QPushButton* test_rect;
     QComboBox* boxes;
     QLabel* info_lang;
+
  private slots:
 
      void clear_field(){txt->clear();}
      void button_func(){main_function();}
      void choice_lang(const QString& str){
-         qDebug() << this << "\t" << lang;
+         //qDebug() << this << "\t" << lang;
          if(str == ""){return;}
          lang = str;
      }
 };
-#endif // MAINWINDOW_H
+#endif // SCANNER_TEXT_H
